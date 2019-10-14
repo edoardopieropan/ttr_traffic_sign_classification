29b1043e19f1cb5ccb79d7f178f6c15e973600ea
edoardopieropan <edoardo.pieropan@studenti.univr.it>
2019-10-14 14:57:20 +0200

    added pca and fixed readme image

--- code/svm_classifier.py
+++ code/svm_classifier.py
@@ -5,21 +5,16 @@ Creazione, training e salvataggio di un classificatore SVM.
 import numpy as np
 import joblib as joblib
 from sklearn.svm import SVC
+from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score
 
-x = np.load('../utils/images_full.npy', allow_pickle=True)
-
-nobj = x.shape[0]
-im_w = x.shape[1]
-im_h = x.shape[2]
-rgb  = x.shape[3]
-
-#reshape
-x = np.reshape(x, (nobj,im_w*im_h*rgb))
-
 # Carico labels e data
-#x = np.load('../utils/features_full.npy', allow_pickle=True)
+x = np.load('../utils/features_full.npy', allow_pickle=True)
 y = np.load('../utils/labels_full.npy')
+
+# PCA
+pca = PCA(n_components=1000)
+x = pca.fit_transform(x)
 
 clf = SVC(kernel="rbf", gamma='auto', C=100, probability=False)
 print('Training classifier...')
